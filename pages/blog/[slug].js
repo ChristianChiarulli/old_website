42d43aaf77e774adcf86cc197f2a65e3c385a4ad
Christian Chiarulli <chris.machine@pm.me>
2022-02-23 21:08:48 -0500

    begin markdown css

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -5,6 +5,7 @@ import marked from 'marked'
 // import Link from 'next/link'
 import Layout from '@/components/Layout'
 import CategoryLabel from '@/components/CategoryLabel'
+import styles from '@/styles/blog/slug.module.css'
 
 // TODO: style this markdown
 
@@ -16,25 +17,25 @@ export default function PostPage({
   return (
     <Layout title={title}>
       {/* <Link href='/blog'>Go Back</Link> */}
-      <div className=' w-full px-10 py-6 bg-dark rounded-lg mt-6 pb-32'>
-        <div className='flex justify-between items-center mt-4'>
-          <h1 className='text-5xl mb-7'>{title}</h1>
+      <div className={styles.article}>
+        <div className={styles.article__container}>
+          <h1 className={styles.article__title}>{title}</h1>
           <CategoryLabel>{category}</CategoryLabel>
         </div>
-        <img src={cover_image} alt='' className='w-full rounded' />
+        <img src={cover_image} alt='' className='w-full rounded-2xl' />
 
-        <div className='flex text-gray-300 justify-between items-center bg-dark p-2 my-8'>
-          <div className='flex items-center'>
+        {/* <div className='flex text-gray-300 justify-between items-center bg-dark p-2 my-8'> */}
+          {/* <div className='flex items-center'> */}
             {/* <img */}
             {/*   src={author_image} */}
             {/*   alt='' */}
             {/*   className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block' */}
             {/* /> */}
             {/* <h4>{author}</h4> */}
-          </div>
-        </div>
+          {/* </div> */}
+        {/* </div> */}
 
-        <div className='blog-text mt-2'>
+        <div className={styles.article__content}>
           <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
         </div>
         <div className='date'>{date}</div>
