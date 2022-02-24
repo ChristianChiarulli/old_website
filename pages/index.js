baf1f223981a7871d6eac28d74eb3508719cd346
Christian Chiarulli <chris.machine@pm.me>
2022-02-23 21:57:22 -0500

    finish porting most of the css

--- pages/index.js
+++ pages/index.js
@@ -5,58 +5,47 @@ import { getPosts } from '@/lib/posts'
 import CategoryList from '@/components/CategoryList'
 import SocialList from '@/components/SocialList'
 import ProjectList from '@/components/ProjectList'
+import styles from '@/styles/home.module.css'
 
 export default function HomePage({ posts }) {
   return (
     <Layout>
-      <div className=' flex-col justify-between'>
-        <div className='flex-col'>
-          <div className='flex relative py-4 container mx-auto justify-start'>
-            <div className='px-10 text-gray-200 w-56'>
-              <div>
-                <img
-                  className='w-40 rounded-full'
-                  src='/site/me.jpg'
-                  alt='LunarVim'
-                />
-              </div>
-            </div>
-            <div>
-              <p className='bg-gray-700 p-3 rounded-2xl text-gray-200 w-72'>
-                Hey I'm Chris, This is where I keep all of my thoughts and links
-                to everything I do on the internet.
-              </p>
-            </div>
+      <div className={styles.home__container}>
+        <div className={styles.home__image__container}>
+          <img
+            className={styles.home__image}
+            src='/site/me.jpg'
+            alt='LunarVim'
+          />
+        </div>
+        <div>
+          <p className={styles.home__intro}>
+            Hey I'm Chris, This is where I keep all of my thoughts and links to
+            everything I do on the internet.
+          </p>
+        </div>
+      </div>
+      <div className={styles.home__post__container}>
+        <div>
+          <h1 className={styles.home__post__title}>RECENTLY PUBLISHED</h1>
+          <div className={styles.home__post__list}>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
           </div>
-          <div className='flex'>
-            <div className='f9 flex-col'>
-              <h1 className='text-xl pt-20 px-10 font-bold'>
-                RECENTLY PUBLISHED
-              </h1>
-              <div className='flex w-full flex-col'>
-                {posts.map((post, index) => (
-                  <Post key={index} post={post} />
-                ))}
-              </div>
-              <Link href='/blog'>
-                <div className='px-8'>
-                  <a className='block px-10 text-center cursor-pointer border font-bold border-gray-700 text-gray-200 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:shadow-outline w-full'>
-                    All Posts
-                  </a>
-                </div>
-              </Link>
+          <Link href='/blog'>
+            <div className={styles.home__post__button__wrapper}>
+              <a className={styles.home__post__button}>All Posts</a>
             </div>
-            <div className='flex-col'>
-              <div className='flex-shrink-0 justify-end mx-1 my flex-col'>
-                <CategoryList
-                  title='FEATURED TOPICS'
-                  categories={['Python', 'JavaScript', 'CSS']}
-                />
-                <SocialList />
-                <ProjectList />
-              </div>
-            </div>
-          </div>
+          </Link>
+        </div>
+        <div>
+          <CategoryList
+            title='FEATURED TOPICS'
+            categories={['Bitcoin', 'Ethereum', 'Linux', 'Neovim']}
+          />
+          <SocialList />
+          <ProjectList />
         </div>
       </div>
     </Layout>
