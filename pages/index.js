de62ac2b0e4abbc71d125bbfb38c308310bd8943
Christian Chiarulli <chris.machine@pm.me>
2022-02-24 23:50:13 -0500

    add sidebar

--- pages/index.js
+++ pages/index.js
@@ -13,40 +13,39 @@ export default function HomePage({ posts }) {
     <Layout>
       <div className={styles.container}>
         {/* <div className={styles.test}> */}
-          <div className={styles.introcard}>
-            <img
-              className={styles.intro__image}
-              src='/site/me.jpg'
-              alt='LunarVim'
-            />
-              <p className={styles.intro__message}>
-                Hey I'm Chris, This is where I keep all of my thoughts and links
-                to everything I do on the internet.
-              </p>
+        <div className={styles.introcard}>
+          <img
+            className={styles.intro__image}
+            src='/site/me.jpg'
+            alt='LunarVim'
+          />
+          <p className={styles.intro__message}>
+            Hey I'm Chris, This is where I keep all of my thoughts and links to
+            everything I do on the internet.
+          </p>
+        </div>
+        <div className={styles.search}>
+          <Search />
+        </div>
+        <div className={styles.posts__container}>
+          <h1 className={styles.posts__title}>RECENTLY PUBLISHED</h1>
+          <div className={styles.posts}>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
           </div>
-          <div className={styles.search}>
-            <Search />
-          </div>
-          <div className={styles.posts__container}>
-              <h1 className={styles.posts__title}>RECENTLY PUBLISHED</h1>
-              <div className={styles.posts}>
-                {posts.map((post, index) => (
-                  <Post key={index} post={post} />
-                ))}
-              </div>
-              <Link href='/blog'>
-                  <a className={styles.home__post__button}>All Posts</a>
-              </Link>
-          </div>
-        {/* </div> */}
-        {/* <div className={styles.home__sidebar__container}> */}
-        {/*   <CategoryList */}
-        {/*     title='FEATURED TOPICS' */}
-        {/*     categories={['Bitcoin', 'Ethereum', 'Linux', 'Neovim']} */}
-        {/*   /> */}
-        {/*   <SocialList /> */}
-        {/*   <ProjectList /> */}
-        {/* </div> */}
+          <Link href='/blog'>
+            <a className={styles.home__post__button}>All Posts</a>
+          </Link>
+        </div>
+        <div className={styles.home__sidebar__container}>
+          <CategoryList
+            title='FEATURED TOPICS'
+            categories={['Bitcoin', 'Ethereum', 'Linux', 'Neovim']}
+          />
+          <SocialList />
+          <ProjectList />
+        </div>
       </div>
     </Layout>
   )
