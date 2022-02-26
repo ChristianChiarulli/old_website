7e53d2e4140641cee8de41d6c22d9dc2a97031a3
ChristianChiarulli <chris.machine@pm.me>
2022-02-26 06:41:53 -0500

    add intro card

--- pages/index.js
+++ pages/index.js
@@ -12,40 +12,41 @@ export default function HomePage({ posts }) {
   return (
     <Layout>
       <div className={styles.container}>
-        {/* <div className={styles.test}> */}
-        <div className={styles.introcard}>
-          <img
-            className={styles.intro__image}
-            src='/site/me.jpg'
-            alt='LunarVim'
-          />
-          <p className={styles.intro__message}>
-            Hey I'm Chris, This is where I keep all of my thoughts and links to
-            everything I do on the internet.
-          </p>
-        </div>
-        <div className={styles.search}>
-          <Search />
-        </div>
-        <div className={styles.posts__container}>
-          <h1 className={styles.posts__title}>RECENTLY PUBLISHED</h1>
-          <div className={styles.posts}>
-            {posts.map((post, index) => (
-              <Post key={index} post={post} />
-            ))}
+        <div className={styles.test}>
+          <div className={styles.introcard}>
+            <img
+              className={styles.intro__image}
+              src='/site/me.jpg'
+              alt='LunarVim'
+            />
+            <p className={styles.intro__message}>
+              I'm Chris, this is where I keep all of my thoughts and links
+              to everything I do on the internet.
+            </p>
           </div>
-          <Link href='/blog'>
-            <a className={styles.home__post__button}>All Posts</a>
-          </Link>
+          <div className={styles.search}>
+            <Search />
+          </div>
+          <div className={styles.posts__container}>
+            <h1 className={styles.posts__title}>RECENTLY PUBLISHED</h1>
+            <div className={styles.posts}>
+              {posts.map((post, index) => (
+                <Post key={index} post={post} />
+              ))}
+            </div>
+            <Link href='/blog'>
+              <a className={styles.home__post__button}>All Posts</a>
+            </Link>
+          </div>
         </div>
-        <div className={styles.home__sidebar__container}>
-          <CategoryList
-            title='FEATURED TOPICS'
-            categories={['Bitcoin', 'Ethereum', 'Linux', 'Neovim']}
-          />
-          <SocialList />
-          <ProjectList />
-        </div>
+          <div className={styles.home__sidebar__container}>
+            <CategoryList
+              title='FEATURED TOPICS'
+              categories={['Bitcoin', 'Ethereum', 'Linux', 'Neovim']}
+            />
+            <SocialList />
+            <ProjectList />
+          </div>
       </div>
     </Layout>
   )
