e328b91862daf6ba682d2e645e5e6c731078a86a
Christian Chiarulli <chris.machine@pm.me>
2022-02-26 02:41:39 -0500

    click outside of search closes menu

--- components/Search.js
+++ components/Search.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import SearchResults from './SearchResults'
 import styles from '@/styles/components/Search.module.css'
@@ -6,6 +6,7 @@ import styles from '@/styles/components/Search.module.css'
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState('')
   const [searchResults, setSearchResults] = useState([])
+  const [isSearchResultsOpen, setIsSearchResultsOpen] = useState(false)
 
   useEffect(() => {
     const getResults = async () => {
@@ -21,13 +22,37 @@ export default function Search() {
     getResults()
   }, [searchTerm])
 
+  const ref = useRef()
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (
+        isSearchResultsOpen &&
+        ref.current &&
+        !ref.current.contains(e.target)
+      ) {
+        setIsSearchResultsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', checkIfClickedOutside)
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener('mousedown', checkIfClickedOutside)
+    }
+  }, [isSearchResultsOpen])
+
   return (
     <div className={styles.search__wrapper}>
       <div className={styles.search__container}>
         <div className={styles.search__input}>
-          <form>
+          <form ref={ref}>
             <input
               type='search'
+              onClick={() => setIsSearchResultsOpen(true)}
               name=''
               id=''
               className={styles.search__form}
@@ -38,7 +63,8 @@ export default function Search() {
 
             <FaSearch className={styles.search__icon} />
           </form>
-          <SearchResults results={searchResults} />
+
+          {isSearchResultsOpen && <SearchResults results={searchResults} />}
         </div>
       </div>
     </div>
