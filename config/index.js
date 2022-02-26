c3a0df38b60dacb1becfe015fe1f91beb91f9f01
Christian Chiarulli <chris.machine@pm.me>
2022-02-26 02:42:01 -0500

    test categories

--- config/index.js
+++ config/index.js
@@ -2,8 +2,10 @@ export const POSTS_PER_PAGE = 6
 
 export const COLOR_KEY = {
   JavaScript: '#d97705',
+  bitcoin: '#d97705',
   CSS: '#3929eb',
   Python: '#049669',
+  ethereum: '#049669',
   PHP: '#7730db',
   Ruby: '#a70b34',
 }
