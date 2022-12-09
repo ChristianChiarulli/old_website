3d9cfffa69878b9e8151719dc8cf9bf3826ea5a1
mahcodes <mhmdali102@bk.ru>
2022-12-09 10:21:37 +0200

    refactor SocialList.js

--- components/SocialList.js
+++ components/SocialList.js
@@ -5,109 +5,73 @@ import {
   FaPatreon,
   FaTwitter,
   FaHeart,
-} from 'react-icons/fa'
-import styles from '@/styles/components/SocialList.module.css'
+} from "react-icons/fa";
+import styles from "@/styles/components/SocialList.module.css";
 
 // TODO: maybe add rss
+
+const SOCIALS = [
+  {
+    title: "Github",
+    href: "https://github.com/ChristianChiarulli",
+    Icon: FaGithub,
+    clr: "#FFFFFF",
+  },
+  {
+    title: "Youtube",
+    href: "https://www.youtube.com/chrisatmachine",
+    Icon: FaYoutube,
+    clr: "#FE0000",
+  },
+  {
+    title: "Twitter",
+    href: "https://twitter.com/chrisatmachine",
+    Icon: FaTwitter,
+    clr: "#1c9bf0",
+  },
+  {
+    title: "Discord",
+    href: "https://discord.gg/Xb9B4Ny",
+    Icon: FaDiscord,
+    clr: "#7289dc",
+  },
+  {
+    title: "Patreon",
+    href: "https://www.patreon.com/chrisatmachine",
+    Icon: FaPatreon,
+    clr: "#E8715C",
+  },
+  {
+    title: "Sponsor",
+    href: "https://github.com/sponsors/ChristianChiarulli",
+    Icon: FaHeart,
+    clr: "#EB48AB",
+  }
+];
 
 export default function SocialList() {
   return (
     <div className={styles.sociallist}>
       <h1 className={styles.sociallist__title}>SOCIAL</h1>
       <ul>
-        <li>
-          <a
-            className={styles.sociallist__item}
-            target='_blank'
-            rel='nofollow noopener noreferrer'
-            href={'https://github.com/ChristianChiarulli'}
-          >
-            <FaGithub
-              color='#FFFFFF'
-              // color='#f34f29'
-              size='30'
-              style={{ marginRight: '.4rem' }}
-            />
-            <span className={styles.sociallist__name}>Github</span>
-          </a>
-        </li>
-        <li>
-          <a
-            className={styles.sociallist__item}
-            target='_blank'
-            rel='nofollow noopener noreferrer'
-            href={'https://www.youtube.com/chrisatmachine'}
-          >
-            <FaYoutube
-              color='#FE0000'
-              size='30'
-              style={{ marginRight: '.4rem' }}
-            />
-            <span className={styles.sociallist__name}>Youtube</span>
-          </a>
-        </li>
-        <li>
-          <a
-            className={styles.sociallist__item}
-            target='_blank'
-            rel='nofollow noopener noreferrer'
-            href={'https://twitter.com/chrisatmachine'}
-          >
-            <FaTwitter
-              color='#1c9bf0'
-              size='30'
-              style={{ marginRight: '.4rem' }}
-            />
-            <span className={styles.sociallist__name}>Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a
-            className={styles.sociallist__item}
-            target='_blank'
-            rel='nofollow noopener noreferrer'
-            href={'https://discord.gg/Xb9B4Ny'}
-          >
-            <FaDiscord
-              color='#7289dc'
-              size='30'
-              style={{ marginRight: '.4rem' }}
-            />
-            <span className={styles.sociallist__name}>Discord</span>
-          </a>
-        </li>
-        <li>
-          <a
-            className={styles.sociallist__item}
-            target='_blank'
-            rel='nofollow noopener noreferrer'
-            href={'https://www.patreon.com/chrisatmachine'}
-          >
-            <FaPatreon
-              color='#E8715C'
-              size='30'
-              style={{ marginRight: '.4rem' }}
-            />
-            <span className={styles.sociallist__name}>Patreon</span>
-          </a>
-        </li>
-        <li>
-          <a
-            className={styles.sociallist__item}
-            target='_blank'
-            rel='nofollow noopener noreferrer'
-            href={'https://github.com/sponsors/ChristianChiarulli'}
-          >
-            <FaHeart
-              color='#EB48AB'
-              size='30'
-              style={{ marginRight: '.4rem' }}
-            />
-            <span className={styles.sociallist__name}>Sponsor</span>
-          </a>
-        </li>
-
+        {SOCIALS.map((social, idx) => (
+          <li key={idx}>
+            <a
+              className={styles.sociallist__item}
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+              href={social.href}
+            >
+              <social.Icon
+                color={social.clr}
+                size="30"
+                style={{ marginRight: ".4rem" }}
+              />
+              <span className={styles.sociallist__name}>{social.title}</span>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
